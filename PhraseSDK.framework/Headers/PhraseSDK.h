5f25ba6fbfd4a14b396b50d8b812b680149f8e83
Frederik Winkelsdorf <winkelsdorf@gmail.com>
2019-12-16 14:14:51 +0100

    Update to 2.0.0

---
+++ PhraseSDK.framework/Headers/PhraseSDK.h
@@ -0,0 +1,15 @@
+//
+//  PhraseSDK.h
+//  PhraseSDK
+//
+//  Copyright © 2019 Dynport GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIDevice.h>
+
+//! Project version number for PhraseSDK.
+FOUNDATION_EXPORT double PhraseSDKVersionNumber;
+
+//! Project version string for PhraseSDK.
+FOUNDATION_EXPORT const unsigned char PhraseSDKVersionString[];
