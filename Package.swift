d8d4ae0968dfd87b2f464dd16a485237b6364658
Frederik Winkelsdorf <winkelsdorf@gmail.com>
2021-01-26 12:06:29 +0100

    Fix Package.swift URL

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PhraseSDK",
-            url: "https://github.com/phrase/ios-sdk/releases/download/3.0.0/PhraseSDK.zip",
+            url: "https://github.com/phrase/ios-sdk/releases/download/3.0.0/PhraseSDK.xcframework.zip",
             checksum: "5a2a2a53d1ae507f02d28d7d4a9238242fa154d3597166542ab6ca4487eb947e"
         ),
     ]
