8a9de00a69787740aeb09f6f49bbbfa8b6f4c3a1
Frederik Winkelsdorf <winkelsdorf@gmail.com>
2022-06-11 11:49:47 +0200

    Update Package.swift for 4.0.1

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PhraseSDK",
-            url: "https://github.com/phrase/ios-sdk/releases/download/4.0.1-rc.1/PhraseSDK.xcframework.zip",
+            url: "https://github.com/phrase/ios-sdk/releases/download/4.0.1/PhraseSDK.xcframework.zip",
             checksum: "ff8e19d1a3ddd21ff3b7ccfb6daa451f2eae3ba5e0cf856009ea81fdcc9da8a1"
         ),
     ]
