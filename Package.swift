2879205bc727fda5f742c2dfe273c308c6d26750
Frederik Winkelsdorf <winkelsdorf@gmail.com>
2023-04-17 21:44:28 +0200

    Release Version 4.2.0

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PhraseSDK",
-            url: "https://github.com/phrase/ios-sdk/releases/download/4.1.0/PhraseSDK.xcframework.zip",
-            checksum: "9d4c419aaa61e0e3c2f5f065b02cfe0e41a97e9d8a1a60adbf9d7eed4f6aed0d"
+            url: "https://github.com/phrase/ios-sdk/releases/download/4.2.0/PhraseSDK.xcframework.zip",
+            checksum: "490d993839affcedb30f30dbfe16c50ca1ed7e7d7f3524a840bca94e4d0c1fc3"
         ),
     ]
 )
