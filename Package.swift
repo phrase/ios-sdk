7ff96f6192d140ad7d91dc9eca7f5f4521ae73f2
Carsten Schlobohm <carsten.schlobohm@apploft.de>
2023-10-11 10:42:39 +0200

    Release Version 4.3.1

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PhraseSDK",
-            url: "https://github.com/phrase/ios-sdk/releases/download/4.3.0/PhraseSDK.xcframework.zip",
-            checksum: "60cf2d09f706e4c5cb19100ef437dcb12322eed598b995354a826c737782566e"
+            url: "https://github.com/phrase/ios-sdk/releases/download/4.3.1/PhraseSDK.xcframework.zip",
+            checksum: "667207641254b1207d150d2343292295f19a7914b678e78fec7d05983774944a"
         ),
     ]
 )
